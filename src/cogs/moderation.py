ffa6edb6de2845296d701ad61518b1133c1d4f8f
SyedAhkam <smahkam57@gmail.com>
2020-07-30 04:09:52 +0530

    Added the verify command

--- src/cogs/moderation.py
+++ src/cogs/moderation.py
@@ -25,6 +25,18 @@ class Moderation(commands.Cog):
         await user.edit(nick=formatted_nickname)
         await ctx.send(f'Successfully changed ``{user.name}\'s`` nickname from ``{user.nick}`` to ``{formatted_nickname}``')
 
+    @commands.command(name='verify', help='Give users the verified role.')
+    @commands.has_any_role(697877262737080390, 697877262737080391)
+    async def verified(self, ctx, user: commands.MemberConverter = None):
+        if not user:
+            await ctx.send('Please provide a user.')
+            return
+
+        verified_role = ctx.guild.get_role(697877261952483477)
+
+        await user.add_roles(verified_role)
+        await ctx.send(f'Successfully added the verified role to ``{user.name}``')
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
