3446839bf98c981b6e552aa6fcb2a2fe0a2c8a54
Syed Ahkam <smahkam57@gmail.com>
2020-10-06 20:31:29 +0530

    Change verification title

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -225,7 +225,7 @@ class Events(commands.Cog):
                 ctx = await self.bot.get_context(message)
 
                 embed = embeds.normal(
-                    verification_doc['verification_followup_message'], 'You\'ve been verified!', ctx)
+                    verification_doc['verification_followup_message'], '**__You just got verified!__**', ctx)
                 await message.channel.send(content=message.author.mention, embed=embed)
                 await message.delete()
 
